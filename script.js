0d946d6672a35a2d723908c2cc2a50af8ea3d9ec
David Samuel <samikendrickjnr@gmail.com>
2020-04-19 13:58:35 +0100

    my task

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-let name = "david";
-console.log(name);
+
+function checkNumber(integar){
+    let result = [];
+    for(let ray=1; ray <=integar; ray++){
+        if(ray % 2 === 0 && ray % 3 === 0 && ray % 5 === 0){
+               result.push("yu-gi-oh");
+            }
+        else if(ray % 2 === 0 && ray % 3 === 0){
+            result.push("yu-gi");
+        }
+        else if(ray % 2 === 0 && ray % 5 === 0){
+            result.push("yu-oh");
+        }
+        else if(ray % 2 === 0){
+               result.push("yu");
+        }
+        else if(ray % 3 === 0 && ray % 5 === 0){
+                result.push("gi-oh");
+        }
+        else if(ray % 3 === 0){
+                result.push("gi");
+        }
+        else if(ray % 5 === 0){
+                result.push("oh");
+        }
+       else{
+           result.push(ray);
+       } 
+    }
+    console.log(result); 
+    return result;
+}    
+   checkNumber(100);
+   checkNumber(20);
