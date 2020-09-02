1d791b2280623d5fe9be2db06e78790cd42f97e9
David Samuel <samikendrickjnr@gmail.com>
2020-09-02 13:46:22 +0100

    1st commit

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 
-function checkNumber(integar){
-    let result = [];
-    for(let ray=1; ray <=integar; ray++){
-        if(ray % 2 === 0 && ray % 3 === 0 && ray % 5 === 0){
-               result.push("yu-gi-oh");
-            }
-        else if(ray % 2 === 0 && ray % 3 === 0){
-            result.push("yu-gi");
-        }
-        else if(ray % 2 === 0 && ray % 5 === 0){
-            result.push("yu-oh");
-        }
-        else if(ray % 2 === 0){
-               result.push("yu");
-        }
-        else if(ray % 3 === 0 && ray % 5 === 0){
-                result.push("gi-oh");
-        }
-        else if(ray % 3 === 0){
-                result.push("gi");
-        }
-        else if(ray % 5 === 0){
-                result.push("oh");
-        }
-       else{
-           result.push(ray);
-       } 
-    }
-    console.log(result); 
-    return result;
-}    
-   checkNumber(100);
-   checkNumber(20);
+// function checkNumber(integar){
+//     let result = [];
+//     for(let ray=1; ray <=integar; ray++){
+//         if(ray % 2 === 0 && ray % 3 === 0 && ray % 5 === 0){
+//                result.push("yu-gi-oh");
+//             }
+//         else if(ray % 2 === 0 && ray % 3 === 0){
+//             result.push("yu-gi");
+//         }
+//         else if(ray % 2 === 0 && ray % 5 === 0){
+//             result.push("yu-oh");
+//         }
+//         else if(ray % 2 === 0){
+//                result.push("yu");
+//         }
+//         else if(ray % 3 === 0 && ray % 5 === 0){
+//                 result.push("gi-oh");
+//         }
+//         else if(ray % 3 === 0){
+//                 result.push("gi");
+//         }
+//         else if(ray % 5 === 0){
+//                 result.push("oh");
+//         }
+//        else{
+//            result.push(ray);
+//        } 
+//     }
+//     console.log(result); 
+//     return result;
+// }    
+//    checkNumber(100);
+//    checkNumber(20);
+
+<script>
+  AOS.init();
+</script>
